8c94050a57339568d7a10372f625af11d0e3d9ef
MehdiOmrani <mehditony17@gmail.com>
2023-01-27 11:42:56 +0100

    buttonclicker

---
+++ Week2/Day 2/Core/Button Clicker/script.js
@@ -0,0 +1,10 @@
+function alertt(){
+    alert("Ninja was liked")
+
+}
+document.getElementById("cookies").onclick = function() {
+    document.getElementById("cookies").style.display = "none";
+}
+btn.addEventListener('click', function handleClick() {
+    btn.textContent = 'logout';
+});
